2fb2953f1d0f8eac82b4d09aba99409d31583ba8
KryTonX <andrasmumm99@yahoo.de>
2017-10-05 20:03:14 +0200

    asdasd
    asdasd

--- TKFF/TKWrapper.cs
+++ TKFF/TKWrapper.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,18 +9,23 @@ using System.Threading.Tasks;
 namespace TKFF
 {
     class TKWrapper
-
-
     {
+        ChromeWrapper browser = null;
         public TKWrapper() {
         Console.WriteLine("Hi!");
-        ChromeWrapper browser = new ChromeWrapper();
+        browser = new ChromeWrapper();
             Console.WriteLine("Going to Kingdoms!");
             browser.LoadUrl("http://www.kingdoms.com");
             Console.WriteLine("Back to google!");
             browser.LoadUrl("http://www.google.de");
             Console.WriteLine("Aaaand back to Kingdoms!");
             browser.LoadUrl("http://www.kingdoms.com");
+        }
+
+        public void Login(String username, String password)
+        {
+            browser.clickElement(By.Id("loginButton"));
+            browser.waitForElement(3,By.Name("email"));
         }
 
     }
